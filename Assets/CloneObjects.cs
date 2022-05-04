0d1d09dd9a05630b09456c947dec430e9255b9d2
jereabu <jeremiasabulafia@gmail.com>
2022-05-04 15:49:49 -0300

    Initial commit

---
+++ Assets/CloneObjects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObjects : MonoBehaviour
+{
+    int i;
+    public GameObject objectToClone;
+    public void InstatiateObject()
+    {
+        while (i != 3) {
+            Instantiate(objectToClone);
+            i++;
+
+        }
+        i = 0;
+    }
+}
